a47f675d9ea508b1a03ed53e8aaf7b0961967b34
Joao Almancio <joao.almancio@globalservs.com>
2020-08-26 19:46:12 -0300

    Exercicio finalizado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Router from "./router/router";
+import { getCurrentPath } from './router/utils/router-utils';
 
 const router = new Router({
   path: 'what-is-vegemite',
@@ -9,8 +10,38 @@ router.create('what-is-vegemite', 'what-is-vegemite');
 router.create('recipes', 'recipes');
 router.create('ingredients', 'ingredients');
 
-
+var firstLoad = true;
 function handleMenuActive() {
-  console.log('ow')
+  let navElements = document.querySelectorAll('#menu [data-page]');
+
+  const renderActive = function () {
+    for (let i = 0; i < navElements.length; i++) {
+      if (navElements[i].getAttribute('data-page') === getCurrentPath()) {
+        navElements[i].classList.add('is-active');
+        if (!firstLoad) {
+          handleHeaderFocus(getCurrentPath());
+        } else if (getCurrentPath() !== router.root.path){
+          handleHeaderFocus(getCurrentPath());
+          firstLoad = false;
+        }
+      } else {
+        navElements[i].classList.remove('is-active');
+      }
+    }
+  }
+
+  for (let i = 0; i < navElements.length; i++) {
+    navElements[i].addEventListener('click', () => {
+      renderActive();
+    })
+  }
+  renderActive();
 }
-handleMenuActive()
+handleMenuActive();
+
+function handleHeaderFocus(dataContent) {
+  let contentElement = document.querySelector(`[data-page-content=${dataContent}]`);
+  let headerElement = contentElement.querySelector('h2');
+  headerElement.tabIndex = 0;
+  headerElement.focus();
+}
